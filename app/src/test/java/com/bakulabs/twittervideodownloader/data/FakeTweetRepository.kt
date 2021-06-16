f6e1cb619d08bb7d896e9bdde646747254ae6db4
Bakoura Ibrahima <bakouraibrahima@gmail.com>
2021-06-16 21:52:15 +0200

    refacto

--- app/src/test/java/com/bakulabs/twittervideodownloader/data/FakeTweetRepository.kt
+++ app/src/test/java/com/bakulabs/twittervideodownloader/data/FakeTweetRepository.kt
@@ -6,11 +6,10 @@ import java.lang.Exception
 class FakeTweetRepository(private var tweets: List<Tweet>): TweetRepository {
     override suspend fun getTweet(id: String): DataResult<Tweet> {
         val tweet = tweets.firstOrNull { it.id.toString() == id }
-        if (tweet != null) {
-            return DataResult.Success(tweet)
-        }
-        else {
-            return DataResult.Error(Exception("Tweet not found"))
+        return if (tweet != null) {
+            DataResult.Success(tweet)
+        } else {
+            DataResult.Error(Exception("Tweet not found"))
         }
     }
 }
